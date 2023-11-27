723b0cedebc4ad6bcf2f1b21974f3341fe81e07d
Farid <farid.hasanaliyev99@gmail.com>
2023-11-27 14:48:43 +0400

    mes5

--- src/main/java/com/example/exercise/ExerciseApplication.java
+++ src/main/java/com/example/exercise/ExerciseApplication.java
@@ -10,7 +10,7 @@ public class ExerciseApplication {
 		SpringApplication.run(ExerciseApplication.class, args);
 		System.out.println("start1");
 		System.out.println("start3");
-		System.out.println("start4");
+		System.out.println("start5");
 
 
 	}
